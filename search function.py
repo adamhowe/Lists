0824a921617cca7a79297df1ad7ee4b4e0a0ce81
Adam-Howe <40769@longroad.ac.uk>
2015-01-08 11:57:19 +0000

    Search a list function

---
+++ search function.py
@@ -0,0 +1,26 @@
+
+
+
+def search(searchList,numbers):
+    found = False
+    counter = 0
+    while not found and counter < len(numbers):
+        if numbers[counter] == searchList:
+            found = True
+        else:
+            counter = counter + 1
+        return found
+            
+                    
+
+#Main Program   
+numbers = ["one","two","three","four","five"]
+searchList = input("Search: ")
+searchList = str.lower(searchList)
+found = search(searchList,numbers)
+
+
+if found:
+    print("Found")
+else:
+    print("Not Found")
